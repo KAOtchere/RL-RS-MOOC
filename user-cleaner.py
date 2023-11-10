6b3cf34bc32294ee29b2142103adafdd9ff4af3d
KAOtchere <kwabena.aboagye@alumni.ashesi.edu.gh>
2023-11-10 10:29:32 -0600

    pd user-filter mod

--- user-cleaner.py
+++ user-cleaner.py
@@ -1,31 +1,13 @@
 #Author: Kwabena Aboagye-Otchere
-import json
+import pandas as pd
 
 def filter_users(input_file, output_file, min_course_depth):
-    """This program filters out students who don't have adequate course depth
-        to train the recommender algorithm.
-        It writes the output to a json file specified in the command line argument"""
+    users = pd.read_json(input_file, lines=True, encoding='utf-8')
     
-    #open the user.json file
-    with open(input_file, 'r') as infile:
-        #list meant to hold the users that meet the filter criteria of k number of courses (currently 4)
-        filtered_users = []
+    filtered_users = users[users['course_order'].apply(len) >= min_course_depth]
 
-        for line in infile:
-            try:
-                user = json.loads(line)
-                if len(user.get("course_order", [])) >= min_course_depth:
-                    filtered_users.append(user)
-            except json.JSONDecodeError:
-                print(f"Failed to parse JSON data in line: {line}")
-
-    with open(output_file, 'w') as outfile:
-        for user in filtered_users:
-            json.dump(user, outfile, separators=(',', ':'), ensure_ascii=False)
-            outfile.write('\n')
-
-    print(f"Filtered users have been written to {output_file}")
-
+    return filtered_users
+    
 if __name__ == "__main__":
     import sys
 
@@ -38,4 +20,7 @@ if __name__ == "__main__":
     output_file = sys.argv[2]
     min_course_depth = int(sys.argv[3])
 
-    filter_users(input_file, output_file, min_course_depth)
+    filtered_users = filter_users(input_file, output_file, min_course_depth)
+    
+    filtered_users.to_json(output_file, lines=True, orient='records', force_ascii=False)
+
